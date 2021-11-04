0673a37982ebdd39ec677d0ef616379242ca1e4a
idealization <sanghwaann@gmail.com>
2021-11-04 23:52:45 +0900

    @idealization login

--- jasmineapp/index.js
+++ jasmineapp/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 // const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
 
@@ -12,6 +13,7 @@ const { User } = require('./models/User');
 // app.use(bodyParser.json);
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 const mongoose = require('mongoose');
@@ -26,9 +28,6 @@ app.get('/', (req, res) => {
 
 app.post('/register', (req, res) => {
 
-	// 회원 가입할 때 필요한 정보들을 client에서 가져오면
-	// 그것들을 데이터베이스에 넣어준다.
-
 	const user = new User(req.body)
 
 	user.save((err, userInfo) => {
@@ -38,6 +37,29 @@ app.post('/register', (req, res) => {
 		})
 	})
 
+})
+
+app.post('/login', (req, res) => {
+
+	User.findOne({ email: req.body.email }, (err, user) => {
+		if (!user) {
+			return res.json({
+				loginSuccess: false,
+				message: "제공된 이메일에 해당하는 유저가 없습니다."
+			})
+		}
+
+		user.comparePassword(req.body.password, (err, isMatch) => {
+			if (!isMatch)
+				return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+			
+			user.generateToken((err, user) => {
+				if (err) return res.status(400).send(err);
+
+				res.cookie('x_auth', user.token).status(200).json({ loginSuccess: true, userId: user._id });
+			})
+		})
+	})
 })
 
 app.listen(port, () => {
--- jasmineapp/models/User.js
+++ jasmineapp/models/User.js
@@ -1,56 +1,75 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
-	name: {
-		type: String,
-		maxlength: 50
-	},
-	email: {
-		type: String,
-		trim: true,
-		unique: 1
-	},
-	password: {
-		type: String,
-		minlength: 4
-	},
-	lastname: {
-		type: String,
-		maxlength: 50
-	},
-	role: {
-		type: Number,
-		default: 0
-	},
-	image: String,
-	token: {
-		type: String
-	},
-	tokenExp: {
-		type: Number
-	}
-})
+    name: {
+        type: String,
+        maxlength: 50,
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1,
+    },
+    password: {
+        type: String,
+        minlength: 4,
+    },
+    lastname: {
+        type: String,
+        maxlength: 50,
+    },
+    role: {
+        type: Number,
+        default: 0,
+    },
+    image: String,
+    token: {
+        type: String,
+    },
+    tokenExp: {
+        type: Number,
+    },
+});
 
 userSchema.pre('save', function (next) {
-	var user = this;
+    var user = this;
 
-	if(user.isModified('password')) {
-		
-		bcrypt.genSalt(saltRounds, function(err, salt) {
-			if (err) return next(err)
+    if (user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err);
 
-			bcrypt.hash(user.password, salt, function(err, hash) {
-				if (err) return next(err)
-				user.password = hash;
-				next();
-			});
-		});
-	}
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
 
-})
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+		cb(null, isMatch);
+    });
+};
 
-const User = mongoose.model('User', userSchema)
-module.exports = {User}
+userSchema.methods.generateToken = function (cb) {
+    var user = this;
+
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+    user.token = token;
+    user.save(function (err, user) {
+        if (err) return cb(err);
+        cb(null, user);
+    });
+};
+
+const User = mongoose.model('User', userSchema);
+module.exports = { User };
