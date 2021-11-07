6d840b1399f1e8e1c60f2e4b30a6ec3e5c2a8ed0
idealization <sanghwaann@gmail.com>
2021-11-07 12:57:15 +0900

    @idealization concurrent

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 
 function LandingPage() {
     useEffect(() => {
-        axios.get('http://localhost:5000/api/hello')
-        .then((response) => console.log(response.data))
+        axios.get('/api/hello').then((response) => {
+            console.log(response.data);
+        });
     }, []);
     return <div>LandingPage</div>;
 }
