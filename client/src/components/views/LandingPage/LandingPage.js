053aad410847ffe46d7d2b7a9e9d16fa22cc2783
idealization <sanghwaann@gmail.com>
2021-11-14 15:40:43 +0900

    @idealization pretty

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -24,62 +24,56 @@ function LandingPage(props) {
 
     return (
         <html lang="en">
-
             <body>
-
                 <div class="site-wrapper">
-
                     <div class="site-wrapper-inner">
-
                         <div class="cover-container">
                             <div class="masthead clearfix">
                                 <div class="inner">
-                                <h3 class="masthead-brand">Cover</h3>
-                                <nav>
-                                    <ul class="nav masthead-nav">
-                                    <li class="active"><a href="#">Home</a></li>
-                                    <li><a href="#">Features</a></li>
-                                    <li><a href="#">Contact</a></li>
-                                    </ul>
-                                </nav>
+                                    <h3 class="masthead-brand">Cover</h3>
+                                    <nav>
+                                        <ul class="nav masthead-nav">
+                                            <li class="active">
+                                                <a href="#">Home</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Features</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Contact</a>
+                                            </li>
+                                        </ul>
+                                    </nav>
                                 </div>
                             </div>
 
                             <div class="inner cover">
                                 <h1 class="cover-heading">Cover your page.</h1>
-                                <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
                                 <p class="lead">
-                                <a href="#" class="btn btn-lg btn-default">Learn more</a>
+                                    Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and
+                                    add your own fullscreen background photo to make it your own.
+                                </p>
+                                <p class="lead">
+                                    <a href="#" class="btn btn-lg btn-default">
+                                        Learn more
+                                    </a>
                                 </p>
                             </div>
 
                             <div class="mastfoot">
                                 <div class="inner">
-                                <p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+                                    <p>
+                                        Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by{' '}
+                                        <a href="https://twitter.com/mdo">@mdo</a>.
+                                    </p>
                                 </div>
                             </div>
-
                         </div>
-
                     </div>
-
                 </div>
             </body>
         </html>
     );
-        {/*<div class="site-wrapper">
-         <div
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                width: '100%',
-                height: '100vh',
-            }}
-        >
-            <h2>시작 페이지</h2>
-            <button onClick={onClickHandler}>로그아웃</button>
-        </div>*/}
 }
 
 export default withRouter(LandingPage);
