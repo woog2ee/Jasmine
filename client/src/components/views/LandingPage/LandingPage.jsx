a438f3b504fd0dbe9eace8b9d79fef0cf255494e
bo-lim <leebolim0701@cau.ac.kr>
2021-11-25 17:24:21 +0900

    @bo-lim css

--- client/src/components/views/LandingPage/LandingPage.jsx
+++ client/src/components/views/LandingPage/LandingPage.jsx
@@ -1,25 +1,15 @@
 import React, { useEffect } from 'react';
-import axios from 'axios';
 import { withRouter } from 'react-router-dom';
 import './cover.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Foot from '../Footer/Footer';
 import Navbar from '../NavBar/NavBar';
+import LandingImg from '../../../img/landing.png'
 
 function LandingPage(props) {
 
     const onHomeClickHandler = () => {
         props.history.push('/home');
-    };
-
-    const onLogoutClickHandler = () => {
-        axios.get('/api/users/logout').then((response) => {
-            if (response.data.success) {
-                props.history.push('/login');
-            } else {
-                alert('로그아웃에 실패했습니다.');
-            }
-        });
     };
 
     return (
@@ -30,17 +20,15 @@ function LandingPage(props) {
                         <div className="cover-container">
                             <Navbar />
                             <div className="inner cover">
-                                <h1 className="cover-heading">Cover your page.</h1>
+                                <img className="testImg" src={LandingImg}/>
                                 <p className="lead">
-                                    Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and
-                                    add your own fullscreen background photo to make it your own.
+                                    아이들의 자신감 비타민,<br/>자스민.
                                 </p>
                                 <p className="lead">
                                     <button className="btn btn-lg btn-default" onClick={onHomeClickHandler}>
                                         시작하기
                                     </button>
                                 </p>
-                                <button onClick={onLogoutClickHandler}>로그아웃</button>
                             </div>
                             <Foot />
                         </div>
