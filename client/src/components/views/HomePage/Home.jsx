2d341cf40b9bc628671a93e257a7ca1209a53d3c
idealization <sanghwaann@gmail.com>
2021-11-26 16:40:22 +0900

    @idealization home flower

--- client/src/components/views/HomePage/Home.jsx
+++ client/src/components/views/HomePage/Home.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from '../../../img/logo.png';
 import '../../../css/Home.css';
 import Rock from "./Rock";
 
 
 function Home() {
+    const [flower, setFlower] = useState(0);
+
+    useEffect(() => {
+        axios.get('/api/report/user', {
+            params: {
+                userFrom: userFrom
+            }
+        }).then((response) => {
+            if (response.data.success) {
+            } else {
+                alert('사용자 정보를 불러오는 데 실패했습니다.');
+            }
+            setFlower((preFlower) => preFlower + response.data.user["flower"]);
+        })
+    })
+
     return (
         <div className="main_board">
             <div className="simpleNavi">
